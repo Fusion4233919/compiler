a2c020b0e2dde0970c9b6954ba3f5315f3d78410
Fan Andong <anthonyfan2018@163.com>
2021-06-03 12:17:28 +0800

    Refactor gen

--- gen/gen.cpp
+++ gen/gen.cpp
@@ -2,6 +2,10 @@
 #include "AST.h"
 
 namespace gen {
+
+    class ValueWrapper {
+
+    };
     
     llvm::LLVMContext llvmContext;
     llvm::Module llvmModule("compiler_module", llvmContext);
@@ -9,8 +13,7 @@ namespace gen {
     static std::unique_ptr<llvm::LLVMContext> TheContext;
     static std::unique_ptr<llvm::Module> TheModule;
     static std::unique_ptr<llvm::IRBuilder<>> Builder;
-    static std::map<std::string, llvm::AllocaInst *> NamedValues;
-    static std::map<std::string, llvm::AllocaInst *> GlobalNamedValues;
+    static std::map<std::string, ValueWrapper*> NamedValues;
 
     static llvm::AllocaInst *CreateEntryBlockAlloca(llvm::BasicBlock *EntryBlock, AST* TypeNode, AST* Var) {
         llvm::IRBuilder<> TempBuilder(EntryBlock, EntryBlock->begin());
@@ -25,93 +28,56 @@ namespace gen {
         return TempBuilder.CreateAlloca(llvm::Type::getInt32Ty(*TheContext), ArrayType, std::string(Var->name));
     }
 
-    static llvm::Value *programCodeGen(const AST *node) {
+    static void SingleGlobalDeclGen(char* Name, DataType Type) {
+        llvm::Constant *InitConst;
+        llvm::Value *Value;
+        if (Type == DataType::integer) {
+            llvm::Type *llvmType = llvm::Type::getInt32Ty(*TheContext);
+            InitConst = llvm::dyn_cast<llvm::Constant>(llvm::ConstantInt::get(llvmType, 0, true));
+            Value = new llvm::GlobalVariable(llvmModule, llvmType, false, llvm::GlobalValue::CommonLinkage, InitConst, std::string(Name));
+        } else if (Type == DataType::string) {
+
+        }
+        ValueWrapper *wvalue = new ValueWrapper(Value, Type);
+        NamedValues[std::string(Name)] = wvalue;
+    }
+
+    static void ArrayGlobalDeclGen(char* Name, DataType Type, int Len) {
+
+    }
+
+    static void GlobalVarDeclGen(AST *Def) {
+        auto Type = Def->children->at(0)->dtype;
+        auto* VarList = Def->children->at(1);
+
+        for (auto* Var : *(VarList->children)) {
+            if (Var->child_num == 0) {
+                SingleGlobalDeclGen(Var->name, Type);
+            } else {
+                ArrayGlobalDeclGen(Var->name, Type, Var->children->at(0)->dvalue.integer);
+            }
+        }
+    }
+
+    static void GlobalVarDeclListGen(AST *DefList) {
+        for (auto* Def : *(DefList->children)) {
+            GlobalVarDeclGen(Def);
+        }
+    }
+
+    static void ProgramGen(AST *node) {
         AST *DefList = node->children->at(0);
         AST *FunDefList = node->children->at(1);
 
-        llvm::BasicBlock *BB = llvm::BasicBlock::Create(*TheContext, "entry");
-        Builder->SetInsertPoint(BB);
+        GlobalVarDeclListGen(DefList);
 
-        GlobalNamedValues.clear();
-        for (AST *Def : *(DefList->children)) {
-            AST *TypeNode = Def->children->at(0);
-            AST *VarList = Def->children->at(1);
-
-            for (AST *Var : *(VarList->children)) {
-                if (TypeNode->dtype == DataType::integer) {
-                    llvm::AllocaInst *Alloca = CreateEntryBlockAlloca(BB, TypeNode, Var);
-                    GlobalNamedValues[std::string(Var->name)] = Alloca;
-                }
+        for (auto* FunDef : *(FunDefList->children)) {
+            if (strcmp(FunDef->name, "_main")) {
+                MainFuncAPICalls();
+                FuncGen(FunDef);
+                return;
             }
+            FuncGen(FunDef);
         }
-
-        
     }
-
-    static llvm::Value *varDefCodeGen(const AST *node) {
-        return nullptr;
-    }
-
-    static llvm::Value *funcCodeGen(const AST *node) {
-        return nullptr;
-    }
-
-    static llvm::Value *litCodeGen(const AST *node) {
-        return nullptr;
-    }
-
-    static llvm::Value *declCodeGen(const AST *node) {
-        return nullptr;
-    }
-
-    static llvm::Value *opCodeGen(const AST *node) {
-        return nullptr;
-    }
-
-    static llvm::Value *funcAttrCodeGen(const AST *node) {
-        return nullptr;
-    }
-
-    static llvm::Value *listCodeGen(const AST *node) {
-        return nullptr;
-    }
-
-
-    static llvm::Value *expCodeGen(const AST *node) {
-        return nullptr;
-    }
-
-    static llvm::Value *lvalueCodeGen(const AST *node) {
-        return nullptr;
-    }
-
-    static llvm::Value *codeGen(const AST *node) {
-        switch (node->ntype) {
-            case none:
-                if (strcmp(node->name, "program") == 0) {
-                    return programCodeGen(node);
-                } else if (strcmp(node->name, "Def")) {
-                    return varDefCodeGen(node);
-                }
-                break;
-            case func:
-                return funcCodeGen(node);
-            case cconst:
-                return litCodeGen(node);
-            case var:
-                return declCodeGen(node);
-            case fvar:
-                return funcAttrCodeGen(node);
-            case list:
-                return listCodeGen(node);
-            case opr:
-                return opCodeGen(node);
-            case Type::exp:
-                return expCodeGen(node);
-            case lvalue:
-                return lvalueCodeGen(node);
-        }
-        return nullptr;
-    }
-
 }
